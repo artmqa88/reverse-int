0e6c95b3d623e19a3e09da08865c372209dd5cba
artmqa88 <artmqa@mail.ru>
2021-03-12 17:37:13 +0300

    fix: complete reverse-int -task

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
-module.exports = function reverse (n) {
-  
-  
+module.exports = function reverseInt (n) {
+    let reverseInt
+
+    if (n < 0) {
+        n = Math.abs(n);
+        reverseInt = n.toString().split('').reverse().join('');
+    }
+    reverseInt = n.toString().split('').reverse().join('');
+    return reverseInt;
 }
+
